66fd32d5ed4114426f405ef593b3b29ecf65dd1e
Daehyun Kim <hatemogi@gmail.com>
2024-03-22 15:43:39 +0900

    현재 디렉터리 열어서 브랜치 이름들 출력

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use git2::Repository;
+use std::env;
 
 fn main() {
-    let repo = match Repository::init("/Users/dante/work/rust-git") {
+    let pwd = env::current_dir().unwrap();
+
+    let repo = match Repository::init(pwd) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to init: {}", e),
     };
-    let branches = repo.branches(None).unwrap().enumerate();
-    for _branch in branches {
-        println!("a branch");
+
+    let branches = repo.branches(None).unwrap();
+    for branch in branches {
+        let b = branch.unwrap();
+        println!("branch => {:?}", b.0.name());
     }
     println!("Hello, world!");
 }
